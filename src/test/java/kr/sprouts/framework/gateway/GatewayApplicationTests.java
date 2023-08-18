809ed50e9a9a0ec5f849672ba6339cecf445d669
Jinwon Seo <jwseo@live.co.kr>
2023-08-18 15:35:04 +0900

    Remove empty test method.

--- src/test/java/kr/sprouts/framework/gateway/GatewayApplicationTests.java
+++ src/test/java/kr/sprouts/framework/gateway/GatewayApplicationTests.java
@@ -1,11 +1,6 @@
 package kr.sprouts.framework.gateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
-
-	@Test
-	void contextLoads() { }
-}
+class GatewayApplicationTests { }
